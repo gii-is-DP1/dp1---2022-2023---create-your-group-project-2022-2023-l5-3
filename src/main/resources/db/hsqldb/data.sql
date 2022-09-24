a2b60cdb5b15c6fb51c328eeb353f4edc5357905
Mario <marsannar2@alum.us.es>
2022-09-24 10:36:58 +0200

    added marsannar2 as a user

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -4,6 +4,9 @@ INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
 -- One owner user, named owner1 with passwor 0wn3r
 INSERT INTO users(username,password,enabled) VALUES ('owner1','0wn3r',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (2,'owner1','owner');
+-- One owner user, named mario with passwor mario
+INSERT INTO users(username,password,enabled) VALUES ('marsannar2','mario',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (7,'marsannar2','owner');
 -- One vet user, named vet1 with passwor v3t
 INSERT INTO users(username,password,enabled) VALUES ('vet1','v3t',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
