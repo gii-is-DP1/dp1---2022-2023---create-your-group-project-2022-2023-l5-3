db28dd1d99fb9cb205a52d7d0430c86952f21abc
barbaat <barbatrejofco@gmail.com>
2022-09-29 11:58:54 +0200

    Probando cambio

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -77,7 +77,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '201
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Betis', '2012-11-11', 2, 11);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Pipo', '2022-11-18', 3, 12);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'BarbaPET', '2022-11-18', 3, 12);
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
