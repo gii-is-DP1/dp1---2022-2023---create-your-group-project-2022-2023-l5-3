81af23a17fb89f45d2d9fef0e4367b287fa0b5b1
marsannar2 <soymarcosr@gmail.com>
2022-10-27 11:49:54 +0200

    Relaciones mazos-cartasPartida

--- src/main/java/org/springframework/samples/petclinic/mazoFinal/MazoFinal.java
+++ src/main/java/org/springframework/samples/petclinic/mazoFinal/MazoFinal.java
@@ -1,11 +1,15 @@
 package org.springframework.samples.petclinic.mazoFinal;
 
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.samples.petclinic.mazo.Mazo;
 import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
@@ -24,5 +28,8 @@ public class MazoFinal extends BaseEntity{
     @Column(name = "cantidad")
     @Max(52)
     private Integer cantidad;
+
+    @OneToMany(mappedBy = "mazos")
+    private Collection<Mazo> mazos;
 
 }
--- src/main/java/org/springframework/samples/petclinic/mazoInicial/MazoInicial.java
+++ src/main/java/org/springframework/samples/petclinic/mazoInicial/MazoInicial.java
@@ -1,12 +1,16 @@
 package org.springframework.samples.petclinic.mazoInicial;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.samples.petclinic.cartasPartida.CartasPartida;
+import org.springframework.samples.petclinic.mazo.Mazo;
 import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
@@ -24,5 +28,7 @@ public class MazoInicial extends BaseEntity{
     @NotNull
     private Integer cantidad = 24;
     //la baraja inicial contiene 24 cartas al inicio del juego
+    @OneToMany
+    private Collection<CartasPartida> cartasPartida;
 
 }
