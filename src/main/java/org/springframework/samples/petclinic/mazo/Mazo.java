df5dbbdfde2c8f22de59ebd04b1eaf895924eab3
jorsilman <jorsilman@alum.us.es>
2022-10-27 09:08:48 +0200

    Modelos conceptuales

---
+++ src/main/java/org/springframework/samples/petclinic/mazo/Mazo.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.petclinic.mazo;
+
+import java.util.Collection;
+
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+import org.springframework.samples.petclinic.cartasPartida.CartasPartida;
+import org.springframework.samples.petclinic.model.BaseEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "mazos")
+public class Mazo extends BaseEntity{
+    
+    @NotNull
+    @Range(min = 0, max = 6)
+    private Integer posicion;
+
+    @NotNull
+    private Integer cantidad;
+
+    @ManyToMany(mappedBy = "mazos")
+    private Collection<CartasPartida> cartasPartida;
+}
