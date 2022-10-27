86e76b6bf44ddfe3446f4d40b0c7061fc47897ef
alvnavriv <alvnavriv@alum.us.es>
2022-10-27 12:23:38 +0200

    Entidad jugador añadido estadisticas

--- src/main/java/org/springframework/samples/petclinic/jugador/Jugador.java
+++ src/main/java/org/springframework/samples/petclinic/jugador/Jugador.java
@@ -1,10 +1,12 @@
 package org.springframework.samples.petclinic.jugador;
 
+
+import java.time.LocalTime;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-
 
 import org.springframework.samples.petclinic.model.Person;
 import org.springframework.samples.petclinic.user.User;
@@ -17,8 +19,29 @@ import lombok.Setter;
 @Setter
 public class Jugador extends Person{
 
+    private Integer partidasJugadas;
+
+    private Integer partidasGanadas;
+
+    private Integer partidasNoGanadas;
+
+    private Integer totalTiempoJugado;
+    
+    private Integer numTotalMovimientos;
+
+    private Integer numTotalPuntos;
+
+    private Integer numMaxMovimientosPartidaGanada;
+
+    private Integer numMinMovimientosPartidaGanada;
+
+    private LocalTime maxTiempoPartidaGanada;
+
+    private LocalTime minTiempoPartidaGanada;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "username", referencedColumnName = "username")
 	private User user;
+
+
 }
