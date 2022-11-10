681b7d985e88496603a41902f432388dfd0c8765
jorsilman <jorsilman@alum.us.es>
2022-11-10 12:10:54 +0100

    Entidad carta terminada con enumerados

--- src/main/java/org/springframework/samples/petclinic/carta/Palo.java
+++ src/main/java/org/springframework/samples/petclinic/carta/Palo.java
@@ -2,6 +2,11 @@ package org.springframework.samples.petclinic.carta;
 
 public enum Palo {
     TREBOLES, CORAZONES, PICAS, DIAMANTES;
-
-    public Color color;
+    public Color getColor(Palo p){
+        if(p == Palo.CORAZONES || p == Palo.DIAMANTES){
+            return Color.ROJO;
+        }else{
+            return Color.NEGRO;
+        }
+    }
 }
