3739d17341dee11341a13d54d1a751a3abe8fe55
jorsilman <jorsilman@alum.us.es>
2022-11-11 18:46:18 +0100

    Enumerado Palo

--- src/main/java/org/springframework/samples/petclinic/carta/Palo.java
+++ src/main/java/org/springframework/samples/petclinic/carta/Palo.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.carta;
 
 public enum Palo {
     TREBOLES, CORAZONES, PICAS, DIAMANTES;
+    
     public Color getColor(Palo p){
         if(p == Palo.CORAZONES || p == Palo.DIAMANTES){
             return Color.ROJO;
